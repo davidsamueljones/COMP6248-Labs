f1363ee7e9782621b370e6b36ac5e27071663052
David Jones <davidsamueljones@icloud.com>
2020-05-17 20:14:07 +0100

    Added trained weights, added figure creation

--- lab_5/lab5.py
+++ lab_5/lab5.py
@@ -3,12 +3,28 @@ import torch
 import torch.nn.functional as F
 import torchbearer
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 from torchvision import transforms
-from torch import optim
 from torch.utils.data import Dataset, DataLoader
 from torchbearer import Trial
-from torch import nn
+from torch import optim, nn, Tensor
+from typing import Tuple
+
+
+def get_figure_dir() -> str:
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    figure_dir = os.path.join(cur_dir, "report", "figures")
+    os.makedirs(figure_dir, exist_ok=True)
+    return figure_dir
+
+
+def save(fig: plt.Figure, name: str):
+    figure_dir = get_figure_dir()
+    fig.tight_layout()
+    fig.savefig(os.path.join(figure_dir, name + ".pgf"))
+    fig.savefig(os.path.join(figure_dir, name + ".pdf"))
 
 
 class MyDataset(Dataset):
@@ -106,10 +122,6 @@ class CNN3C(nn.Module):
         idx = torch.stack([idxx, idxy]).unsqueeze(0)
         idx = torch.repeat_interleave(idx, repeats=x.shape[0], dim=0)
         x = torch.cat([x, idx], dim=1)
-        # for xi in x[0]:
-        #     print("")
-        #     print(xi)
-        # exit()
         out = F.relu(self.conv1(x))
         out = F.relu(self.conv2(out))
         out = self.mp1(out)
@@ -130,7 +142,7 @@ def train_model(
     optimiser = optim.Adam(model.parameters())
 
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    trial = Trial(model, optimiser, loss_function, metrics=["loss", "accuracy"]).to(
+    trial = Trial(model, optimiser, loss_function, metrics=["accuracy"]).to(
         device
     )
     trial.with_generators(train_loader, val_generator=val_loader)
@@ -143,7 +155,7 @@ def train_model(
 def test_model(model: nn.Module, test_loader: DataLoader):
     loss_function = nn.MSELoss()
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    trial = Trial(model, None, loss_function, metrics=["loss", "accuracy"]).to(device)
+    trial = Trial(model, None, loss_function, metrics=["accuracy"]).to(device)
     trial.with_generators(train_loader, test_generator=test_loader)
     results = trial.evaluate(data_key=torchbearer.TEST_DATA)
     return results, trial
@@ -157,7 +169,51 @@ def seed(n: int = None):
     print("Seed:", n)
 
 
+def im_plot(data: Tensor):
+    fig, ax = plt.subplots(1, 1)
+    ax.imshow(data)
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+    ax.tick_params(
+        axis="both",
+        which="both",
+        bottom=False,
+        top=False,
+        left=False,
+        right=False,
+        labelbottom=False,
+    )
+    return fig, ax
+
+def plot_params(params: Tuple[Tensor, Tensor], color: str):
+    x0, x1 = 0, 40
+    y0, y1 = 0, 40
+
+    m, c = params
+    # Correct m, c for plotting origin
+    m, c, = -m, (-c + 40)
+    px0, px1 = x0 - 5, x1 + 5
+    x = torch.linspace(px0, px1, px1 - px0)
+    y = m * x + c
+    plt.plot(x, y, color=color)
+    plt.xlim([x0, x1 - 1])
+    plt.ylim([x0, y1 - 1])
+
 if __name__ == "__main__":
+    plot = True
+
+    if plot:
+        plot_idx = 3
+        matplotlib.use("pgf")
+        matplotlib.rcParams.update(
+            {
+                "pgf.texsystem": "pdflatex",
+                "font.family": "serif",
+                "text.usetex": True,
+                "pgf.rcfonts": False,
+            }
+        )
+
     os.chdir(os.path.dirname(__file__))
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -166,18 +222,6 @@ if __name__ == "__main__":
     val_data = MyDataset(size=500, random_offset=33333)
     test_data = MyDataset(size=500, random_offset=99999)
 
-    import matplotlib.pyplot as plt
-    fig, ax = plt.subplots(1, 3)
-    idxx = torch.repeat_interleave(
-        torch.arange(-20, 20, dtype=torch.float).unsqueeze(0) / 40.0,
-        repeats=40,
-        dim=0,
-    )
-    idxy = idxx.clone().t()
-    ax[0].imshow(train_data[0][0][0])
-    ax[1].imshow(idxx)
-    ax[2].imshow(idxy)
-    plt.show()
     # Ex 1
     print("Ex 1:")
     seed(n=7)
@@ -192,9 +236,18 @@ if __name__ == "__main__":
     if train:
         train_model(model, train_loader, val_loader, weights_path)
     else:
-        model.load_state_dict(torch.load(weights_path, map_location=torch.device(device)))
+        model.load_state_dict(
+            torch.load(weights_path, map_location=torch.device(device))
+        )
         model = model.eval()
     test_model(model, test_loader)
+    if plot:
+        im, params = test_data[plot_idx]
+        est_params = model(im.unsqueeze(0))[0]
+        fig, ax = im_plot(im.squeeze())
+        plot_params(params, "green")
+        plot_params(est_params.detach(), "red")
+        save(fig, "baseline")
 
     # Build the model
     print("Ex 2:")
@@ -210,9 +263,18 @@ if __name__ == "__main__":
     if train:
         train_model(model, train_loader, val_loader, weights_path)
     else:
-        model.load_state_dict(torch.load(weights_path, map_location=torch.device(device)))
+        model.load_state_dict(
+            torch.load(weights_path, map_location=torch.device(device))
+        )
         model = model.eval()
     test_model(model, test_loader)
+    if plot:
+        im, params = test_data[plot_idx]
+        est_params = model(im.unsqueeze(0))[0]
+        fig, ax = im_plot(im.squeeze())
+        plot_params(params, "green")
+        plot_params(est_params.detach(), "red")
+        save(fig, "pooling")
 
     # Build the model
     print("Ex 3:")
@@ -228,6 +290,23 @@ if __name__ == "__main__":
     if train:
         train_model(model, train_loader, val_loader, weights_path)
     else:
-        model.load_state_dict(torch.load(weights_path, map_location=torch.device(device)))
+        model.load_state_dict(
+            torch.load(weights_path, map_location=torch.device(device))
+        )
         model = model.eval()
     test_model(model, test_loader)
+    if plot:
+        im, params = test_data[plot_idx]
+        est_params = model(im.unsqueeze(0))[0]
+        fig, ax = im_plot(im.squeeze())
+        plot_params(params, "green")
+        plot_params(est_params.detach(), "red")
+        print(params, est_params)
+        save(fig, "coord_conv")
+        # Examples of positional channels
+        idxx = torch.repeat_interleave(
+            torch.arange(-20, 20, dtype=torch.float).unsqueeze(0) / 40.0, repeats=40, dim=0,
+        )
+        idxy = idxx.clone().t()
+        save(im_plot(idxx)[0], "xx")
+        save(im_plot(idxy)[0], "xy")
